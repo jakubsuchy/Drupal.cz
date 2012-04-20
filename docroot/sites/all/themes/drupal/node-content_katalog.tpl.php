b40b55d855a2bd199c9816d171874faff1e879ba
Petr Okurek <pokurek@redweb.cz>
2012-04-20 12:27:02 +0200

    Refs #16. Delete contemplate for catalog webu.
    Copy contemplate tpl content into node-content_katalog.tpl and make some
    minor html modifications.

--- docroot/sites/all/themes/drupal/node-content_katalog.tpl.php
+++ docroot/sites/all/themes/drupal/node-content_katalog.tpl.php
@@ -1,7 +1,35 @@
 <div class="article <?php if ($sticky) { print " sticky"; } ?><?php if (!$status) { print " node-unpublished"; } ?>">
   <?php if ($submitted) { ?><span class="submitted"><?php print $submitted?></span><?php } ?>
   <div class="pcontent">
-    <?php print $content?>
+    <div class="clear-block">
+      <div class="katalogtext">
+        <h3>O webu</h3>
+        <div class="field field-type-text field-field-popis-webu">
+          <div class="field-items">
+            <?php foreach ((array)$field_popis_webu as $item) { ?>
+              <div class="field-item"><?php print $item['view'] ?></div>
+            <?php } ?>
+          </div>
+        </div>
+        <div class="field field-type-link field-field-url-0">
+          <div class="field-items">
+            <?php foreach ((array)$field_url_0 as $item) { ?>
+              <div class="field-item"><?php print $item['view'] ?></div>
+            <?php } ?>
+          </div>
+        </div>
+      </div> <!--katalog text end -->
+  
+      <div class="field field-type-image field-field-screenshoty">
+        <h3 class="field-label">Screenshot</h3>
+        <div class="field-items">
+          <?php foreach ((array)$field_screenshoty as $item) { ?>
+            <div class="field-item"><a class="thickbox" href="/<?php print $item['filepath'];?>"><?php print theme('imagecache', 'katalogwebu', $item['filepath']) ?></a></div>
+          <?php } ?>
+        </div>
+      </div>
+    </div>
+
     <h3>Tagy</h3>
     <?php
       _pathauto_include();
